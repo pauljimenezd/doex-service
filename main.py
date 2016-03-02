6ed041cbb2be75544b51a4546fe9ca65b3182a13
Paul J <me@pauljimenez.com.do>
2016-03-02 00:31:04 -0400

    Implementing memcache for the requests and code reorganisation.
    - Caching rates and currencies requests in memcache.
    - Code and URI reordering.

--- main.py
+++ main.py
@@ -15,7 +15,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import json
+
 import webapp2
+from webapp2_extras import routes
+
 import handlers
 import tasks
 
@@ -33,23 +37,49 @@ def handle_500(request, response, exception):
     response.set_status(500)
 
 
+def error_handler(request, response, exception):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.content_type = 'application/json; charset=utf-8'
+    response.set_status(exception.code)
+    response.out.write(json.dumps({'code': exception.code, 'title': exception.title, 'detail': str(exception.wsgi_response)}))
+
+
 app = webapp2.WSGIApplication([
-    webapp2.Route(r'/currencies', handler=handlers.Currencies, name='currency'),
-    webapp2.Route(r'/currencies/<code:[a-z]{3}>', handler=handlers.Currencies, name='currencies-list'),
-    webapp2.Route(r'/currencies/<currency:[a-z]{3}>/rates', handler=handlers.Rates, name='currency-rates-list'),
-    # Rates route
-    webapp2.Route(r'/rates', handler=handlers.Rates, handler_method='today', name='rates-today'),
-    webapp2.Route(r'/rates/today', handler=handlers.Rates, handler_method='today', name='rates-today'),
-    webapp2.Route(r'/rates/now', handler=handlers.Rates, handler_method='now', name='rates-now'),
-    webapp2.Route(r'/rates/this-month', handler=handlers.Rates, handler_method='this_month', name='rates-this-mont'),
-    webapp2.Route(r'/rates/<currency:[a-z]{3}>', handler=handlers.Rates, handler_method='today', name='rates-today-currency'),
-    webapp2.Route(r'/rates/<currency:[a-z]{3}>/today', handler=handlers.Rates, handler_method='today', name='rates-today-currency'),
-    webapp2.Route(r'/rates/<currency:[a-z]{3}>/now', handler=handlers.Rates, handler_method='now', name='rates-now'),
-    webapp2.Route(r'/rates/<currency:[a-z]{3}>/this-month', handler=handlers.Rates, handler_method='this_month', name='rates-this-mont'),
-    # Tasks route
-    webapp2.Route(r'/tasks/rates/update', handler=tasks.RatesUpdateTasks, handler_method='update', name='rates-update'),
-    webapp2.Route(r'/tasks/rates/sync', handler=tasks.RatesUpdateTasks, handler_method='synchronize', name='rates-sync')
+    webapp2.Route('/', webapp2.RedirectHandler, defaults={'_uri': 'http://pauljimenez.com.do/doex'}),
+    routes.PathPrefixRoute('/api', [
+        # Currencies routes
+        routes.PathPrefixRoute('/currencies', [
+            webapp2.Route('', handler=handlers.Currencies, name='currencies'),
+            webapp2.Route(r'/<code:[a-z]{3}>', handler=handlers.Currencies, name='currency'),
+            webapp2.Route(r'/<currency:[a-z]{3}>/rates', handler=handlers.Rates, name='currency-rates-list'),
+            webapp2.Route(r'/<currency:[a-z]{3}>/rates/<date:[0-9]{4}-[0-9]{2}-[0-9]{2}>', handler=handlers.Rates, name='currency-rates-date'),
+        ]),
+        # Rates routes
+        routes.PathPrefixRoute(r'/rates', [
+            webapp2.Route('', handler=handlers.Rates, name='rates'),
+            webapp2.Route(r'/<currency:[a-z]{3}>', handler=handlers.Rates, name='currency-rates'),
+            webapp2.Route(r'/<date:[0-9]{4}-[0-9]{2}-[0-9]{2}>', handler=handlers.Rates, name='rates-date'),
+            webapp2.Route(r'/<date:[0-9]{4}-[0-9]{2}-[0-9]{2}>/<currency:[a-z]{3}>', handler=handlers.Rates, name='rates-date-currency'),
+            webapp2.Route(r'/<:[0-9]{4}-[0-9]{2}-[0-9]{2}>/<code:[a-z]{3}>/currency', handler=handlers.Currencies, name='rate-currency'),
+        ]),
+        # Tasks routes
+        routes.PathPrefixRoute('/tasks', [
+            routes.PathPrefixRoute('/rates', [
+                webapp2.Route('/update', handler=tasks.RatesUpdateTasks, handler_method='update', name='task-rate-update'),
+                webapp2.Route('/sync', handler=tasks.RatesUpdateTasks, handler_method='synchronize', name='task-rate-sync'),
+            ])
+        ]),
+    ]),
 ], debug=False)
 
-app.error_handlers[404] = handle_404
+app.error_handlers[404] = error_handler
+app.error_handlers[400] = error_handler
 app.error_handlers[500] = handle_500
+
+
+def main():
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
