b554e5180fc6d5d5548ee2cdd0efd25599222249
Paul J <me@pauljimenez.com.do>
2018-03-15 20:15:38 -0400

    Hotfix 0.0.11
    - Modifications to the request user agent.
    - Changing the cron hour to run.

--- tasks.py
+++ tasks.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
-from urllib2 import urlopen
+from urllib2 import urlopen, Request
 from datetime import date
 from google.appengine.api import memcache
 
@@ -20,54 +20,66 @@ class RatesUpdateTasks(webapp2.RequestHandler):
         columns = json.loads(Config.get_by_id('rates_columns').value or '{}')
         last_row = Config.get_by_id('rates_last_row')
 
+        hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+               'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+               'Accept-Encoding': 'none',
+               'Accept-Language': 'en-US,en;q=0.8',
+               'Connection': 'keep-alive'}
+
+        req = Request(source, headers=hdr)
+
         initial_row = int(last_row.value) or 3
         last_processed = initial_row
 
         threshold = 100
 
         if source:
-            source_file = urlopen(source)
-            with xlrd.open_workbook(file_contents=source_file.read()) as book:
-                # Getting the sheet to work with
-                sheet = book.sheet_by_name(sheet_name=sheet_name)
-                total_records = sheet.nrows
-                last_processed = initial_row
+            source_file = urlopen(req)
+            try:
+                with xlrd.open_workbook(file_contents=source_file.read()) as book:
+                    # Getting the sheet to work with
+                    sheet = book.sheet_by_name(sheet_name=sheet_name)
+                    total_records = sheet.nrows
+                    last_processed = initial_row
 
-                if initial_row < total_records:
-                    rate_list = []
-                    processed = False
-                    for rowIndex in range(initial_row, ((initial_row + threshold) if (initial_row + threshold) < total_records else total_records)):
-                        year = int(sheet.cell_value(rowx=rowIndex, colx=0))
-                        month = int(MONTHS.get((sheet.cell_value(rowx=rowIndex, colx=1) or '').lower().strip(), None))
-                        day = int(sheet.cell_value(rowx=rowIndex, colx=2))
+                    if initial_row < total_records:
+                        rate_list = []
+                        processed = False
+                        for rowIndex in range(initial_row, ((initial_row + threshold) if (initial_row + threshold) < total_records else total_records)):
+                            year = int(sheet.cell_value(rowx=rowIndex, colx=0))
+                            month = int(MONTHS.get((sheet.cell_value(rowx=rowIndex, colx=1) or '').lower().strip(), None))
+                            day = int(sheet.cell_value(rowx=rowIndex, colx=2))
 
-                        try:
-                            rate_date = date(year, month, day) if year and month and day else None
-                        except ValueError:
-                            continue
+                            try:
+                                rate_date = date(year, month, day) if year and month and day else None
+                            except ValueError:
+                                continue
 
-                        if rate_date:
-                            for currency, colIndex in columns.items():
-                                col_value = sheet.cell_value(rowx=rowIndex, colx=colIndex)
+                            if rate_date:
+                                for currency, colIndex in columns.items():
+                                    col_value = sheet.cell_value(rowx=rowIndex, colx=colIndex)
 
-                                if col_value is not '':
-                                    # Verify if already exists a rate in the db
-                                    if not Rate.query(Rate.currency == ndb.Key('Currency', currency), Rate.date == rate_date).count():
-                                        rate_list.append(Rate(currency=ndb.Key('Currency', currency), date=rate_date, value=col_value))
-                                        processed = True
+                                    if col_value is not '':
+                                        # Verify if already exists a rate in the db
+                                        if not Rate.query(Rate.currency == ndb.Key('Currency', currency), Rate.date == rate_date).count():
+                                            rate_list.append(Rate(currency=ndb.Key('Currency', currency), date=rate_date, value=col_value))
+                                            processed = True
 
-                        last_processed = rowIndex
-                        if processed:
-                            if (last_processed - initial_row) >= threshold:
-                                break
+                            last_processed = rowIndex
+                            if processed:
+                                if (last_processed - initial_row) >= threshold:
+                                    break
 
-                    if len(rate_list) > 0:
-                        ndb.put_multi(rate_list)
-                        memcache.flush_all()
+                        if len(rate_list) > 0:
+                            ndb.put_multi(rate_list)
+                            memcache.flush_all()
 
-                    # Updating the "last_row" processed
-                    last_row.value = str(last_processed + 1)
-                    last_row.put()
+                        # Updating the "last_row" processed
+                        last_row.value = str(last_processed + 1)
+                        last_row.put()
+            except Exception, e:
+                return self.response.write(u'Error while sync: %s' % e)
 
         return self.response.write(u'Total processed: ' + str((last_processed + 1) - initial_row))
 
@@ -76,6 +88,14 @@ class RatesUpdateTasks(webapp2.RequestHandler):
         sheet_name = Config.get_by_id('rates_sheet_name').value or None
         columns = json.loads(Config.get_by_id('rates_columns').value or '{}')
         last_row = Config.get_by_id('rates_last_sync_row')
+        hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+               'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+               'Accept-Encoding': 'none',
+               'Accept-Language': 'en-US,en;q=0.8',
+               'Connection': 'keep-alive'}
+
+        req = Request(source, headers=hdr)
 
         initial_row = int(last_row.value) or 3
         last_processed = initial_row
@@ -83,46 +103,49 @@ class RatesUpdateTasks(webapp2.RequestHandler):
         threshold = 20
 
         if source:
-            source_file = urlopen(source)
-            with xlrd.open_workbook(file_contents=source_file.read()) as book:
-                # Getting the sheet to work with
-                sheet = book.sheet_by_name(sheet_name=sheet_name)
-                total_records = sheet.nrows
-                last_processed = initial_row
+            source_file = urlopen(req)
+            try:
+                with xlrd.open_workbook(file_contents=source_file.read()) as book:
+                    # Getting the sheet to work with
+                    sheet = book.sheet_by_name(sheet_name=sheet_name)
+                    total_records = sheet.nrows
+                    last_processed = initial_row
 
-                if initial_row < total_records:
-                    rate_list = []
-                    for rowIndex in range(initial_row, total_records):
-                        processed = False
-                        year = int(sheet.cell_value(rowx=rowIndex, colx=0))
-                        month = int(MONTHS.get((sheet.cell_value(rowx=rowIndex, colx=1) or '').lower().strip(), None))
-                        day = int(sheet.cell_value(rowx=rowIndex, colx=2))
+                    if initial_row < total_records:
+                        rate_list = []
+                        for rowIndex in range(initial_row, total_records):
+                            processed = False
+                            year = int(sheet.cell_value(rowx=rowIndex, colx=0))
+                            month = int(MONTHS.get((sheet.cell_value(rowx=rowIndex, colx=1) or '').lower().strip(), None))
+                            day = int(sheet.cell_value(rowx=rowIndex, colx=2))
 
-                        try:
-                            rate_date = date(year, month, day) if year and month and day else None
-                        except ValueError:
-                            continue
+                            try:
+                                rate_date = date(year, month, day) if year and month and day else None
+                            except ValueError:
+                                continue
 
-                        if rate_date:
-                            for currency, colIndex in columns.items():
-                                col_value = sheet.cell_value(rowx=rowIndex, colx=colIndex)
+                            if rate_date:
+                                for currency, colIndex in columns.items():
+                                    col_value = sheet.cell_value(rowx=rowIndex, colx=colIndex)
 
-                                if col_value is not '':
-                                    rate_list.append(Rate(currency=ndb.Key('Currency', currency), date=rate_date, value=col_value))
-                                    processed = True
+                                    if col_value is not '':
+                                        rate_list.append(Rate(currency=ndb.Key('Currency', currency), date=rate_date, value=col_value))
+                                        processed = True
 
-                        last_processed = rowIndex
-                        if processed:
-                            threshold -= 1
-                            if threshold == 0:
-                                break
+                            last_processed = rowIndex
+                            if processed:
+                                threshold -= 1
+                                if threshold == 0:
+                                    break
 
-                    if len(rate_list) > 0:
-                        ndb.put_multi(rate_list)
-                        memcache.flush_all()
+                        if len(rate_list) > 0:
+                            ndb.put_multi(rate_list)
+                            memcache.flush_all()
 
-                    # Updating the "last_row" processed
-                    last_row.value = str(last_processed + 1)
-                    last_row.put()
+                        # Updating the "last_row" processed
+                        last_row.value = str(last_processed + 1)
+                        last_row.put()
+            except Exception, e:
+                self.response.write(str(e))
 
         return self.response.write(u'Last processed line: ' + str(last_processed + 1))
